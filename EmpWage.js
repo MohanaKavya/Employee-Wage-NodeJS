8e00513bcba473ceed273d26ec86256602fb4809
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-11-11 21:45:48 +0530

    Modified : Calculation of EMployee Wage for Maximum Days or Hours per Month is MOdified

--- EmpWage.js
+++ EmpWage.js
@@ -9,13 +9,14 @@
         console.log("Employee is Present");
     }
 }
-//  UC_4 Calculating monthly employee wage based on part time or full time work
+//  UC_5 Calculating monthly employee wage based on part time or full time work and max working hours or days
     const IS_PART_TIME = 0;
     const IS_FULL_TIME = 1;
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20;
     const NO_OF_WORKING_DAYS = 20;
+    const MAX_HOURS_MONTHLY = 100;
 
     function getWorkingHours(employeeCheck) {
         switch (employeeCheck) {
@@ -26,9 +27,11 @@
         }
     }
     let employeeHours = 0;
-    for (let day = 0; day < NO_OF_WORKING_DAYS; day++) {
+    let employeeWorkingDays = 0;
+    while (employeeHours <= MAX_HOURS_MONTHLY && employeeWorkingDays <= NO_OF_WORKING_DAYS) {
+        employeeWorkingDays++;
         let employeeCheck = Math.floor(Math.random() * 10) % 2;
         employeeHours += getWorkingHours(employeeCheck);
     }
     let employeeWage = employeeHours * WAGE_PER_HOUR;
-    console.log("Working Hours : " + employeeHours + ", Employee wage for 20 Days: $" + employeeWage);
+    console.log("Working Hours : " + employeeHours + ", Working Days : " + employeeWorkingDays + ", Employee wage for 20 Days: $" + employeeWage);
